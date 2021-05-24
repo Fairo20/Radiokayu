2db3d9b6117abd482f69a923b07e4047e334b6d0
Connor Nicholls <CRNicholls20@gmail.com>
2021-05-23 20:57:56 -0700

    i forgot to capitalize the m in Main

--- Main.py
+++ Main.py
@@ -1,26 +1,15 @@
 import discord
-import logging
-import json
 import os
 from discord.ext import commands
 
+client = commands.Bot(command_prefix="%")
 
 token = os.getenv('DISCORD_BOT_TOKEN')
-
-client = commands.Bot(command_prefix="%")
 
 @client.event
 async def on_ready():
     await client.change_presence(status = discord.Status.idle, activity = discord.Game('Your favorite singing neko'))
-    print('Welcome to Radiokayu!')
-
-#@client.event
-#async def on_message(message):
-#   if message.author == client.user:
-#        return
-
-#    if message.content.startswith('%hello'):
-#        await message.channel.send('Hello!')
+    print(f'Welcome to Radiokayu!')
 
 @client.command()
 async def hello(ctx) :
